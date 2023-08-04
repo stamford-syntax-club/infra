310beb597685fb24e164f09318fe2093a091bc38
chinathaip <cartoonabe@gmail.com>
2023-08-04 15:48:38 +0700

    organize multiple services to multiple docker-compose file & fix readme & start and stop dev script

---
+++ start_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# start haproxy & create stamford network
+docker compose -f haproxy/docker-compose.dev.yaml up -d
+
+# start s3 storage services
+docker compose -f s3/docker-compose.dev.yaml up -d
+
+# start prometheus services
+docker compose -f prometheus/docker-compose.dev.yaml up -d
+
+# start stamford center services
+docker compose -f stamfordcenter/docker-compose.dev.yaml up -d
