4e621f365bd7bf25cb9f604f228cc459cb210408
chinathaip <cartoonabe@gmail.com>
2023-08-18 11:02:01 +0700

    feat: add node-exporter for host metrics & rename folder to monitring

--- stop_dev.sh
+++ stop_dev.sh
@@ -6,8 +6,8 @@ docker compose -f stamfordcenter/docker-compose.dev.yaml down
 # stop s3 storage services
 docker compose -f s3/docker-compose.dev.yaml down
 
-# stop prometheus services
-docker compose -f prometheus/docker-compose.dev.yaml down
+# stop monitoring services
+docker compose -f monitoring/docker-compose.dev.yaml down
 
 # stop haproxy & destroy stamford network
 docker compose -f haproxy/docker-compose.dev.yaml down
