604b670cb92dc287993baa9f90807c636d83d162
chinathaip <cartoonabe@gmail.com>
2023-09-23 15:19:04 +0700

    add to init script

--- stop_dev.sh
+++ stop_dev.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # stop stamford center services
-docker compose -f stamfordcenter/docker-compose.dev.yaml down
+docker compose -f stamfordcenter/docker-compose.dev.yaml down -v
 
 # stop s3 storage services
-docker compose -f s3/docker-compose.dev.yaml down
+docker compose -f s3/docker-compose.dev.yaml down -v 
 
 # stop prometheus services
-docker compose -f prometheus/docker-compose.dev.yaml down
+docker compose -f prometheus/docker-compose.dev.yaml down -v
 
 # stop onboarding docs services
-docker compose -f onboarding/docker-compose.dev.yaml down
+docker compose -f onboarding/docker-compose.dev.yaml down -v
+
+# stop admin services
+docker compose -f admin/docker-compose.dev.yaml down -v
 
 # stop redis services
 docker compose -f redis/docker-compose.dev.yaml down -v
 
 # stop haproxy & destroy stamford network
-docker compose -f haproxy/docker-compose.dev.yaml down
+docker compose -f haproxy/docker-compose.dev.yaml down -v
