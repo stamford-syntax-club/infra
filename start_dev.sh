65006c5076c03aca401de8e2895b26a7ade3e713
chinathaip <cartoonabe@gmail.com>
2024-01-29 00:03:23 +0700

    infra improvement
    fix redis to use haproxy network
    
    create a separate redis network for internal usage
    
    allow direct file retrieval with files subdomain (limited permission configured in config.json)
    
    set container names + restart policy
    
    fix: start redis service before the services that use it
    
    update course compose tags
    
    enable submit course review in api gateway
    
    configure HAProxy rate limit
    
    change cc image tag
    
    add beta web, adjust beta env files
    
    configure haproxy for review site
    
    expose gateway to haproxy
    
    add edit
    
    use docker dns
    
    change to compose-web:pr
    
    upgrade version for  api-gateway and review
    
    fix typo
    
    typo on review
    
    separate coursecompose deployment from this pr

--- start_dev.sh
+++ start_dev.sh
@@ -9,14 +9,14 @@ docker compose -f s3/docker-compose.yaml up -d
 # start monitoring services
 docker compose -f monitoring/docker-compose.yaml up -d
 
+# start redis services
+docker compose -f redis/docker-compose.yaml up -d
+
 # start stamford center services (run .dev file)
 docker compose -f stamfordcenter/docker-compose.dev.yaml up -d
 
 # start admin services
 docker compose -f admin/docker-compose.yaml up -d
-
-# start redis services
-docker compose -f redis/docker-compose.yaml up -d
 
 # start portainer services
 docker compose -f portainer/docker-compose.yaml up -d
